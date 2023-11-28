269ff7680b61817ddb102c1337c20f782d1bfea1
Jonas Simoen <jonas.simoen@ugent.be>
2023-11-28 12:53:43 +0100

    feat(FT-8): points
    commit 529646d4bbc31f31a71413b04bcb2f639906f093
    Author: Jonas Simoen <jonas.simoen@ugent.be>
    Date:   Thu Nov 23 15:22:38 2023 +0100
    
        feat(week): validation endpoint
    
    commit 53770759b73d75325d1159c1c53c6a2872773074
    Author: Jonas Simoen <jonas.simoen@ugent.be>
    Date:   Sun Oct 22 23:15:25 2023 +0200
    
        feat(points): user team points of certain week
    
    commit 66982eb7afc8bbbcdc98b80cd23cdedefb368466
    Author: Jonas Simoen <jonas.simoen@ugent.be>
    Date:   Thu Nov 23 11:31:33 2023 +0100
    
        fix stats

--- src/controllers/Week.ts
+++ src/controllers/Week.ts
@@ -1,129 +1,128 @@
 import { prisma } from "@db/client"
 import { ProcessState } from "@prisma/client";
-import { max } from "lodash";
 
 export const GetWeeksHandler = async (req: any, rep: any) => {
-		const weeks = await prisma.week.findMany({
-				orderBy: {
-						id: 'asc'
-				}
-		});
-		rep.send(weeks);
+	const weeks = await prisma.week.findMany({
+		orderBy: {
+			id: 'asc'
+		}
+	});
+	rep.send(weeks);
 }
 
 export const GetWeekHandler = async (req: any, rep: any) => {
-		const week = await prisma.week.findUnique({
-				where: {
-						id: +req.params.id
-				}
-		});
-		rep.send(week);
+	const week = await prisma.week.findUnique({
+		where: {
+			id: +req.params.id
+		}
+	});
+	rep.send(week);
 }
 
 export const PutWeekHandler = async (req: any, rep: any) => {
-		const week = await prisma.week.update({
-				where: {
-						id: +req.params.id
-				},
-				data: {
-						deadlineDate: req.body.deadlineDate
-				}
-		});
-		rep.send(week);
+	const week = await prisma.week.update({
+		where: {
+			id: +req.params.id
+		},
+		data: {
+			deadlineDate: req.body.deadlineDate
+		}
+	});
+	rep.send(week);
 }
 
 export const PostWeeksHandler = async (req: any, rep: any) => {
-		const week = await prisma.week.create({
-				data: {
-						id: +req.body.id,
-						deadlineDate: req.body.deadlineDate
-				}
-		});
-		rep.send(week);
+	const week = await prisma.week.create({
+		data: {
+			id: +req.body.id,
+			deadlineDate: req.body.deadlineDate
+		}
+	});
+	rep.send(week);
 }
 
 export const PostWeekValidateHandler = async (req: any, rep: any) => {
-		const statsSumPoints = await prisma.statistic.groupBy({
-				where: {
-						match: {
-								weekId: +req.params.id
-						}
-				},
-				by: ['playerId'],
-				_sum: {
-						points: true,
-				}
-		});
+	const statsSumPoints = await prisma.statistic.groupBy({
+		where: {
+			match: {
+				weekId: +req.params.id
+			}
+		},
+		by: ['playerId'],
+		_sum: {
+			points: true,
+		}
+	});
 
-		const [updatedWeek, ...other] = await prisma.$transaction([
-				prisma.week.update({
-						where: {
-								id: +req.params.id,
-						},
-						data: {
-								validated: true
-						}
-				}),
-				...statsSumPoints.map((sumStatPlayer: any) =>
-						prisma.player.update({
-								where: {
-										id: sumStatPlayer.playerId,
-								},
-								data: {
-										points: {
-												increment: sumStatPlayer._sum.points
-										}
-								}
-						})	
-				),
-				prisma.match.updateMany({
-						where: {
-								weekId: +req.params.id,
-						},
-						data: {
-								status: ProcessState.VALIDATED,
-						}
-				}),
-		]);
-		rep.send(updatedWeek);
+	const [updatedWeek, ...other] = await prisma.$transaction([
+		prisma.week.update({
+			where: {
+				id: +req.params.id,
+			},
+			data: {
+				validated: true
+			}
+		}),
+		...statsSumPoints.map((sumStatPlayer: any) =>
+			prisma.player.update({
+				where: {
+					id: sumStatPlayer.playerId,
+				},
+				data: {
+					points: {
+						increment: sumStatPlayer._sum.points
+					}
+				}
+			})
+		),
+		prisma.match.updateMany({
+			where: {
+				weekId: +req.params.id,
+			},
+			data: {
+				status: ProcessState.VALIDATED,
+			}
+		}),
+	]);
+	rep.send(updatedWeek);
 }
 
 export const DeleteWeekHandler = async (req: any, rep: any) => {
 
 }
 export const GetDeadlineInfoHandler = async (req: any, rep: any) => {
-		const weeks = await prisma.week.findMany({
-				orderBy: {
-						id: 'asc'
-				}
-		});
-		const deadlineWeek = await prisma.week.findFirst({
-				where: {
-						deadlineDate: {
-								gte: new Date()
-						}
-				},
-				orderBy: {
-						deadlineDate: 'asc',
-				}
-		});
-		const displayWeek = await prisma.week.findFirst({
-				where: {
-						deadlineDate: {
-								lt: deadlineWeek?.deadlineDate
-						}
-				},
-				orderBy: {
-						deadlineDate: 'desc'
-				}
-		});
-		rep.send({
-				deadlineInfo: {
-						deadlineDate: deadlineWeek?.deadlineDate,
-						deadlineWeek: deadlineWeek?.id,
-						displayWeek: displayWeek?.id,
-						endWeek: weeks[weeks.length - 1].id,
-				},
-				weeks
-		});
+	const weeks = await prisma.week.findMany({
+		orderBy: {
+			id: 'asc'
+		}
+	});
+	const deadlineWeek = await prisma.week.findFirst({
+		where: {
+			deadlineDate: {
+				gte: new Date()
+			}
+		},
+		orderBy: {
+			deadlineDate: 'asc',
+		}
+	});
+	const displayWeek = await prisma.week.findFirst({
+		where: {
+			deadlineDate: {
+				lt: deadlineWeek?.deadlineDate
+			}
+		},
+		orderBy: {
+			deadlineDate: 'desc'
+		}
+	});
+	rep.send({
+		deadlineInfo: {
+			deadlineDate: deadlineWeek?.deadlineDate,
+			deadlineWeek: deadlineWeek?.id,
+			displayWeek: displayWeek?.id,
+			endWeek: weeks[weeks.length - 1].id,
+		},
+		weeks
+	});
 }
