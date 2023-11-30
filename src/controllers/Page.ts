631e0bc3cd2dd4e1024672f2b194ba1b839c1254
jonassimoen <jonas.simoen@ugent.be>
2023-11-30 18:02:18 +0100

    feat(FT-25): pages
    * feat(pages): initial api setup
    
    * feat(pages): include all CRUD operations

---
+++ src/controllers/Page.ts
@@ -0,0 +1,81 @@
+import { prisma } from "@/db/client";
+
+export const GetPagesHandler = async (req: any, rep: any) => {
+	const pages = await prisma.page.findMany({
+		where: {
+			slug: {
+				contains: req.query.slug
+			}
+		},
+		include: {
+			translation: true
+		}
+	});
+
+	rep.send(pages);
+};
+export const GetPageHandler = async (req: any, rep: any) => {
+	const pages = await prisma.page.findMany({
+		where: {
+			slug: {
+				contains: req.query.slug
+			}
+		},
+		include: {
+			translation: true
+		}
+	});
+
+	rep.send(pages);
+};
+
+export const PostPageHandler = async (req: any, rep: any) => {
+	const pages = await prisma.page.create({
+		data: {
+			slug: req.body.slug,
+			translation: {
+				create: req.body.translation,
+			}
+		},
+	});
+
+	rep.send({ msg: 'Pagina gemaakt.' });
+};
+
+export const PutPageHandler = async (req: any, rep: any) => {
+	const pages = await prisma.page.update({
+		where: {
+			id: +req.params.id,
+		},
+		data: {
+			slug: req.body.slug,
+			translation: {
+				upsert: req.body.translation.map((tl: any) => ({
+					where: {
+						id: tl.id,
+					},
+					update: {
+						langCode: tl.langCode,
+						body: tl.body,
+					},
+					create: {
+						langCode: tl.langCode,
+						body: tl.body,
+					}
+				})),
+			}
+		}
+	});
+
+	rep.send({ msg: 'Pagina gewijzigd.' })
+};
+
+export const DeletePageHandler = async (req: any, rep: any) => {
+	const pages = await prisma.page.delete({
+		where: {
+			id: +req.params.id
+		}
+	});
+	
+	rep.send({ msg: 'Pagina verwijderd' });
+};
---
+++ src/routers/Page.ts
@@ -0,0 +1,45 @@
+import { DeletePageHandler, GetPageHandler, PostPageHandler, PutPageHandler } from "@/controllers/Page";
+import { RequireUser } from "@/middleware/RequireUser";
+import { PostPageSchema, PutPageSchema } from "@/types/body-schema";
+import { FastifyPluginAsync } from "fastify";
+
+export const PublicPageRouter: FastifyPluginAsync = async server => {
+	server.route({
+		method: 'GET',
+		url: '',
+		preHandler: RequireUser,
+		handler: GetPageHandler,
+		schema: {
+			querystring: {
+				slug: { type: 'string' }
+			}
+		}
+	});
+
+	server.route({
+		method: 'POST',
+		url: '',
+		preHandler: RequireUser,
+		handler: PostPageHandler,
+		schema: {
+			body: PostPageSchema
+		}
+	});
+
+	server.route({
+		method: 'PUT',
+		url: '/:id',
+		preHandler: RequireUser,
+		handler: PutPageHandler,
+		schema: {
+			body: PutPageSchema
+		}
+	});
+
+	server.route({
+		method: 'DELETE',
+		url: '/:id',
+		preHandler: RequireUser,
+		handler: DeletePageHandler,
+	});
+}
