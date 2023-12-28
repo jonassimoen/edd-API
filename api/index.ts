aa4ee5ab408b3b06a1eae82086372b5d61d8e301
Jonas Simoen <jonas.simoen@ugent.be>
2023-12-28 13:15:11 +0100

    deploy: vercel

---
+++ api/index.ts
@@ -0,0 +1,69 @@
+import fastify from "fastify";
+import cookies from "@fastify/cookie";
+import cors from "@fastify/cors";
+import { AdminRouter, PublicRouter } from "@routers/Main";
+import HttpError from "@utils/HttpError";
+
+import dotenv from "dotenv";
+import { deserializeUser } from "@utils/DeserializeUser";
+import fastifyStatic from "@fastify/static";
+import path from "path";
+dotenv.config();
+
+export const server = fastify({
+  logger: {
+    level: "info",
+  },
+  disableRequestLogging: true,
+});
+
+server.addHook("preHandler", deserializeUser);
+
+server.register(cors, {
+  origin:
+    process.env.ENV === ("production" || "prod")
+      ? process.env.CORS_ORIGIN
+      : "*",
+  credentials: true,
+});
+server.register(cookies, {
+  hook: "onRequest",
+});
+
+server.register(fastifyStatic, {
+  root: path.join(__dirname, "../static"),
+  prefix: "/api/static/",
+  list: true,
+  index: false,
+});
+
+server.register(PublicRouter, { prefix: "/api" });
+server.register(AdminRouter, { prefix: "/api" });
+
+server.get("/ping", (req: any, res: any) => {
+  res.status(200).send();
+});
+
+server.setErrorHandler((err, req, rep) => {
+  if (err instanceof HttpError) {
+    rep.status(err.statusCode || 500).send({
+      statusCode: err.statusCode || 500,
+      message: err.message,
+    });
+  } else {
+    rep.status(500).send(err);
+  }
+});
+
+server.listen({ host: "0.0.0.0", port: +(process.env.PORT || 8080) }, (err, address) => {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log(`Server listening at ${address}`);
+});
+
+export default async (req: any, res: any) => {
+  await server.ready();
+  server.server.emit("request", req, res);
+};
