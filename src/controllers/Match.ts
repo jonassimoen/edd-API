e8277d9ffec4cba9e2ba5072a450fa84a5f5a3d2
Jonas Simoen <jonas.simoen@ugent.be>
2024-06-15 17:11:50 +0200

    order of calculation review

--- src/controllers/Match.ts
+++ src/controllers/Match.ts
@@ -141,12 +141,12 @@ export const PostRecalculateMatchPoints = async (req: any, rep: any) => {
 		})),*/
 		// Recalculation logic
 		await prisma.$queryRaw`CALL "recalculateMatchPoints"(${match?.id})`
-		// Update player and team points
-		await prisma.$queryRaw`CALL "calculateTeamAndPlayerPoints"(${match?.weekId})`
 		// Captain - Vice Captain points multipliers (Triple Captain / Vice victory)
 		await prisma.$queryRaw`CALL "processViceCaptainAndBoosters"(${match?.weekId})`,
 		// HiddenGem - GoalRush points multipliers
 		await prisma.$queryRaw`CALL "processPlayerBoosters"(${match?.weekId})`
+		// Update player and team points
+		await prisma.$queryRaw`CALL "calculateTeamAndPlayerPoints"(${match?.weekId})`
 	}, {
 		maxWait: 5000,
 		timeout: 10000,
